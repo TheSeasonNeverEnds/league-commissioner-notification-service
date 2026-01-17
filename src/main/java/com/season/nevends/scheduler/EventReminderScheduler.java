324b5aec9f1b463b98196422b8c124802fe8b2a8
aaronr33-dev <aaronm.robinson97@gmail.com>
2026-01-17 18:37:05 -0500

    TSNE-72: adding event scheduler and local server port

---
+++ src/main/java/com/season/nevends/scheduler/EventReminderScheduler.java
@@ -0,0 +1,14 @@
+package com.season.nevends.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EventReminderScheduler {
+
+    // TODO: create a scheduler to pull league events from database and then
+    //  send out a reminder email/text/push to commissioners and league players
+}
