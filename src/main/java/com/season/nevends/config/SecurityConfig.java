0f3b29bc619549ee482452bf2fa20378f08fb643
aaronr33-dev <aaronm.robinson97@gmail.com>
2026-06-22 17:52:10 -0400

    TSNE-72: adding annotation to configuration file

--- src/main/java/com/season/nevends/config/SecurityConfig.java
+++ src/main/java/com/season/nevends/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package com.season.nevends.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Profile("!local")
+@Profile("dev")
+@Configuration
 public class SecurityConfig {
 
     @Bean
